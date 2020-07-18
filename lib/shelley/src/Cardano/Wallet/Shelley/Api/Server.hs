12d700b8d796f22d48693deaa7dd8cac1fdd9eeb
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-07-18 04:39:16 +0000

    Extract out function `listStakePools_`.

--- lib/shelley/src/Cardano/Wallet/Shelley/Api/Server.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Api/Server.hs
@@ -184,17 +184,18 @@ server byron icarus shelley spl ntp =
 
     stakePools :: Server (StakePools n ApiStakePool)
     stakePools =
-        (\case
-            Just (ApiT stake) -> liftHandler $ listStakePools spl stake
-            Nothing -> Handler $ throwE $ apiError err400 QueryParamMissing $ mconcat
-                [ "The stake intended to delegate must be provided as a query "
-                , "parameter as it affects the rewards and ranking."
-                ]
-
-        )
+        listStakePools_
         :<|> joinStakePool shelley (knownPools spl)
         :<|> quitStakePool shelley
         :<|> delegationFee shelley
+      where
+        listStakePools_ = \case
+            Just (ApiT stake) -> liftHandler $ listStakePools spl stake
+            Nothing -> Handler $ throwE $ apiError err400 QueryParamMissing $
+                mconcat
+                [ "The stake intended to delegate must be provided as a query "
+                , "parameter as it affects the rewards and ranking."
+                ]
 
     byronWallets :: Server ByronWallets
     byronWallets =
